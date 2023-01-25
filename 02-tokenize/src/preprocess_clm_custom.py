0c5081cca216fbfa464214137be1937382be5fdf
arunprsh <arunprsh@amazon.com>
2023-01-25 04:19:26 +0000

    wip

--- 02-tokenize/src/preprocess_clm_custom.py
+++ 02-tokenize/src/preprocess_clm_custom.py
@@ -26,7 +26,6 @@ LOCAL_INPUT_PATH = '/opt/ml/processing/input'
 # LOCAL_OUTPUT_PATH is mapped to S3 output location where we want to save the processed input data (COVID articles)
 LOCAL_OUTPUT_PATH = '/opt/ml/processing/output'
 MAX_LENGTH = 512
-CHUNK_SIZE = 128
 N_GPUS = 1
 
 # Re-create GPT2 tokenizer using the saved custom vocabulary from the previous job
@@ -46,4 +45,40 @@ train_test_splits = dataset.train_test_split(shuffle=True, seed=123, test_size=0
 data_splits = DatasetDict({'train': train_test_splits['train'], 
                            'validation': train_test_splits['test']})
 logger.info(f'Data splits: {data_splits}')
+
+
+
+def tokenize(element):
+    outputs = tokenizer(element['text'], 
+                        truncation=True, 
+                        max_length=MAX_LENGTH, 
+                        return_overflowing_tokens=True, 
+                        return_length=True)
+    input_batch = []
+    for length, input_ids in zip(outputs['length'], outputs['input_ids']):
+        if length == context_length:
+            input_batch.append(input_ids)
+    return {'input_ids': input_batch}
+
+# Tokenize dataset
+logger.info('Tokenizing dataset splits')
+num_proc = int(os.cpu_count()/N_GPUS)
+logger.info(f'Total number of processes = {num_proc}')
+tokenized_datasets = data_splits.map(tokenize, 
+                                     batched=True, 
+                                     num_proc=num_proc,
+                                     remove_columns=data_splits['train'].column_names)
+logger.info(f'Tokenized datasets: {tokenized_datasets}')
+
+# Save tokenized datasets to local disk (EBS volume)
+logger.info(f'Saving tokenized datasets to local disk {LOCAL_OUTPUT_PATH}')
+tokenized_datasets.save_to_disk(f'{LOCAL_OUTPUT_PATH}')
+
+# Validate if datasets were saved correctly
+logger.info('Validating if datasets were saved correctly')
+reloaded_dataset = datasets.load_from_disk(f'{LOCAL_OUTPUT_PATH}')
+logger.info(f'Reloaded dataset: {reloaded_dataset}')
+
+
+
 
